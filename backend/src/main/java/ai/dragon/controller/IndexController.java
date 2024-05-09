716bbc1679db9f6c0fc752be9a61725d99e60f0d
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2024-05-09 13:57:07 +0000

    Init frontend vue.js

--- backend/src/main/java/ai/dragon/controller/IndexController.java
+++ backend/src/main/java/ai/dragon/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+    @GetMapping("/ok")
     public String showUserList(Model model) {
         return "index";
     }
