72f6b127bb35ed189c16e2790032be76203b65a7
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2024-05-09 14:05:23 +0000

    Add missing tasks npm

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Awesome {{ msg }}</h1>
+    <h1>Awesome! {{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
