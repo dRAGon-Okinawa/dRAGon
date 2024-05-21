4ac06200e58958bd64261523ad77d56b95618776
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2024-05-21 19:44:40 +0000

    Adding lombok

--- backend/src/main/java/ai/dragon/config/DataProperties.java
+++ backend/src/main/java/ai/dragon/config/DataProperties.java
@@ -3,8 +3,13 @@ package ai.dragon.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "dragon.data")
+@Getter
+@Setter
 public class DataProperties {
     private String path;
     private String db;
@@ -12,21 +17,5 @@ public class DataProperties {
     public DataProperties() {
         path = ":temp:";
         db = ":memory:";
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getDb() {
-        return db;
-    }
-
-    public void setDb(String db) {
-        this.db = db;
     }
 }
