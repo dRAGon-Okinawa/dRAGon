ad383ca720ee800a367bbbfbcc959e58052b3a4c
Arnaud Mengus <amengus@immonot.com>
2024-05-30 14:51:44 +0000

    refactor: Renaming SiloType to IngestorType

---
+++ backend/src/main/java/ai/dragon/enumeration/IngestorType.java
@@ -0,0 +1,25 @@
+package ai.dragon.enumeration;
+
+public enum IngestorType {
+    LOCAL("LOCAL");
+
+    private String value;
+
+    IngestorType(String value) {
+        this.value = value;
+    }
+
+    public static IngestorType fromString(String text) {
+        for (IngestorType type : IngestorType.values()) {
+            if (type.value.equalsIgnoreCase(text)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
