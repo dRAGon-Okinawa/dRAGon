e1d97c5218acee4cc815c4df6e9d7984533a620f
Arnaud Mengus <amengus@immonot.com>
2024-05-30 14:52:50 +0000

    refactor: Update SiloIngestorJobRequest to use Lombok annotations

--- backend/src/main/java/ai/dragon/job/silo/SiloIngestorJobRequest.java
+++ backend/src/main/java/ai/dragon/job/silo/SiloIngestorJobRequest.java
@@ -1,24 +1,21 @@
 package ai.dragon.job.silo;
 
 import java.util.UUID;
+
 import org.jobrunr.jobs.lambdas.JobRequest;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
-@Accessors(fluent = true)
-@Getter
-@Setter
+@Data
+@Builder
+@Jacksonized
 public class SiloIngestorJobRequest implements JobRequest {
     private UUID uuid;
 
     @Override
     public Class<SiloIngestorJobHandler> getJobRequestHandler() {
         return SiloIngestorJobHandler.class;
-    }
-
-    public static SiloIngestorJobRequest create() {
-        return new SiloIngestorJobRequest();
     }
 }
