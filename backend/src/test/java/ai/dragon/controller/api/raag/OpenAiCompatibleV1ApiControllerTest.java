b2620efd270f61e2139d56e4320f8acc312a1bba
Arnaud Mengus <amengus@immonot.com>
2024-06-11 10:08:30 +0000

    Fix OpenAiCompatibleV1ApiControllerTest

--- backend/src/test/java/ai/dragon/controller/api/raag/OpenAiCompatibleV1ApiControllerTest.java
+++ backend/src/test/java/ai/dragon/controller/api/raag/OpenAiCompatibleV1ApiControllerTest.java
@@ -1,10 +1,12 @@
 package ai.dragon.controller.api.raag;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -13,17 +15,28 @@ import org.springframework.test.context.ActiveProfiles;
 import com.theokanning.openai.model.Model;
 import com.theokanning.openai.service.OpenAiService;
 
+import ai.dragon.entity.FarmEntity;
+import ai.dragon.repository.FarmRepository;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class OpenAiCompatibleV1ApiControllerTest {
     @LocalServerPort
     private int serverPort;
 
+    @Autowired
+    private FarmRepository farmRepository;
+
     @Test
     void listModels() throws Exception {
+        farmRepository.deleteAll();
+        FarmEntity farm = new FarmEntity();
+        farm.setRaagIdentifier("awesome-raag");
+        farmRepository.save(farm);
         OpenAiService service = new OpenAiService("TODO_PUT_KEY_HERE",
                 String.format("http://localhost:%d/api/raag/v1/", serverPort));
         List<Model> models = service.listModels();
         assertFalse(models.isEmpty());
+        assertEquals(1, models.size());
     }
 }
