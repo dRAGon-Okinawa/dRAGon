a9a868ae6067c80c0c899449e84c996ccd5fe6b6
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2024-07-03 20:50:10 +0000

    Updating port 1984 to 8519

--- backend/src/main/java/ai/dragon/listener/DragonApplicationListener.java
+++ backend/src/main/java/ai/dragon/listener/DragonApplicationListener.java
@@ -38,7 +38,7 @@ public class DragonApplicationListener implements ApplicationListener<ContextRef
         System.out.println("================================================");
         System.out.println(String.format("APP URL\t\t : %s://%s:%d/", scheme, host, port));
         System.out.println(String.format("Swagger UI\t : %s://%s:%d/api/swagger-ui.html", scheme, host, port));
-        System.out.println(String.format("JobRunr\t\t : %s://%s:%d/", scheme, host, 1984));
+        System.out.println(String.format("JobRunr\t\t : %s://%s:%d/", scheme, host, 8519));
         System.out.println("================================================");
     }
 }
