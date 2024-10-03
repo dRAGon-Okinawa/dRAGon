607afe9391b14f3cf4b1672c66510ac6c733ec0f
Arnaud Mengus <amengus@immonot.com>
2024-10-03 12:42:13 +0000

    Refactor AbstractTest to include logging of canRunOpenAiRelatedTests and canRunSearXNGRelatedTests

--- backend/src/test/java/ai/dragon/junit/AbstractTest.java
+++ backend/src/test/java/ai/dragon/junit/AbstractTest.java
@@ -1,8 +1,12 @@
 package ai.dragon.junit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 public abstract class AbstractTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractTest.class);
+
     @Value("${OPENAI_API_KEY:}")
     private String openaiApiKey;
 
@@ -10,10 +14,14 @@ public abstract class AbstractTest {
     private Boolean dragonCicd;
 
     protected boolean canRunOpenAiRelatedTests() {
-        return Boolean.TRUE.equals(dragonCicd) || openaiApiKey != null && !openaiApiKey.isEmpty();
+        boolean canRunOpenAiRelatedTests = Boolean.TRUE.equals(dragonCicd) || openaiApiKey != null && !openaiApiKey.isEmpty();
+        LOGGER.info("canRunOpenAiRelatedTests: {}", canRunOpenAiRelatedTests);
+        return canRunOpenAiRelatedTests;
     }
 
     protected boolean canRunSearXNGRelatedTests() {
-        return dragonCicd == null || Boolean.FALSE.equals(dragonCicd);
+        boolean canRunSearXNGRelatedTests = dragonCicd == null || Boolean.FALSE.equals(dragonCicd);
+        LOGGER.info("canRunSearXNGRelatedTests: {}", canRunSearXNGRelatedTests);
+        return canRunSearXNGRelatedTests;
     }
 }
